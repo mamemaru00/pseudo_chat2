8b873f1d769cd5e2c116e1121415beae48c32cf7
mamemaru00 <k.shouhei0711@gmail.com>
2023-10-09 16:12:24 +0900

    ファイル作成

--- src/routes/web.php
+++ src/routes/web.php
@@ -16,3 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// チャットルーム一覧の表示
+Route::get('/', [ChatRoomController::class, 'index'])->name('chatRooms.index');
+//チャットルームの新規作成
+// Route::post('/chatRooms/store', [ChatRoomController::class, 'store'])->name('chatRooms.store');
+// チャットルーム名の編集
+// チャットルームの削除
+// ユーザー名の編集 
+
+// チャットメッセージの表示
+// Route::get('/chatMessage/{id}', [ChatMessageController::class, 'show'])->name('chatMessages.show');
+// チャットメッセージの送信
+// チャットメッセージの自動返信を生成
+// ユーザーチャットメッセージの判別
