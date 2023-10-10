fef109299b9e05d737827ce0053fae475c1ae5bf
katsuyma <k.shouhei0711@gmail.com>
2023-10-10 16:48:58 +0900

    モデルとマイグレイとファイルの設定

--- src/app/Models/ChatRooms.php
+++ src/app/Models/ChatRooms.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class ChatRooms extends Model
 {
     use HasFactory;
+
+    use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'room_name',
+    ];
+
+    public function chatMessages()
+    {
+        return $this->hasMany(ChatMessages::class);
+    }
 }
