ffeddb4abd051891d45d3ac9261d2c48a1f3d395
katsuyma <k.shouhei0711@gmail.com>
2023-10-13 17:44:22 +0900

    フロント作成

--- src/app/Http/Controllers/ChatMessageController.php
+++ src/app/Http/Controllers/ChatMessageController.php
@@ -15,11 +15,13 @@ class ChatMessageController extends Controller
 
     // チャットメッセージの送信
     public function store(Request $request) {
+        // dd($request->all());  // 追加
         $chatMessage = new ChatMessages();  // 変数名を修正
         $chatMessage->chat_message = $request->chat_message;
         // $chatMessage->user_id = $request->user_id;  // マイグレーションにはuser_idが存在しないので削除
-        $chatMessage->chat_room_id = $request->chat_room_id;
+        $chatMessage->chat_room_id = '1';
         $chatMessage->is_system_message = $request->is_system_message ?? false;  // 追加
+        // dd($chatMessage);  // 追加
         $chatMessage->save();
 
         return redirect()->route('ChatMessages.show');
