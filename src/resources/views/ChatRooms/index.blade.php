f6b2ccc0f38f69b5c2c9fe0cd48a1ee671e5c445
mamemaru00 <k.shouhei0711@gmail.com>
2023-10-18 15:56:44 +0900

    一覧フロントの追加

--- src/resources/views/ChatRooms/index.blade.php
+++ src/resources/views/ChatRooms/index.blade.php
@@ -6,33 +6,53 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>疑似チャット一覧</title>
+    <link rel="stylesheet" href="{{ asset('/css/ChatList.css') }}">
 </head>
 
 <body>
     <h1>疑似チャット一覧</h1>
 
-    {{-- ユーザー名表示 --}}
-    <p>ユーザー名：{{ $users->user_name }}</p>
-    {{-- 新規作成ボタン作成 storeで新規作成 --}}
-    {{-- <a href={{ route('chatRooms.store') }}>新規作成</a> --}}
-    {{-- ルートのnameで指定する --}}
-    {{-- <form action="/ChatRooms/store" method="POST"> --}}
-    <form action="{{ route('ChatRooms.store') }}" method="POST">
-        @csrf
-        <button type="submit">新規作成</button>
-    </form>
-
-    {{-- ルーム一覧表示 --}}
-    <ul>
-        @foreach ($chat_rooms as $chat_room)
-            <li>
-                {{-- ルーム詳細ページへのリンク --}}
-                <a href={{ route('ChatMessages.show', ['id' => $chat_room->id]) }}>
-                    {{ $chat_room->room_name }}
-                </a>
-            </li>
-        @endforeach
-    </ul>
+    <div class="chatlist_background">
+        <div class="chatlist_create_button">
+            <form action="/ChatRooms/store" method="POST">
+                @csrf
+                <button type="submit">新規作成</button>
+            </form>
+        </div>
+        <div class="chatlist_positon">
+            <div class="chatlist_background_color">
+                <div class="chatlist_user_name">
+                    <p>ユーザー名</p>
+                    <div class="chatlist_user_name_position">
+                        <p>{{ $users->user_name }}</p>
+                        <div class="chatlist_user_name_edit">
+                            <div class="chatlist_edit">
+                                <button href="/">編集</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="chatlist_display">
+                    <ul>
+                        @foreach ($chat_rooms as $chat_room)
+                            {{-- ルーム詳細ページへのリンク --}}
+                            <div class="chatlist_room_list">
+                                <a href={{ route('ChatMessages.show', ['id' => $chat_room->id]) }}>
+                                    {{ $chat_room->room_name }}
+                                    <div class="chatlist_edit">
+                                        <button href="/">編集</button>
+                                    </div>
+                                    <div class="chatlist_delete">
+                                        <button href="/">削除</button>
+                                    </div>
+                                </a>
+                            </div>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
